6c4018a329ac74ececab50b24b53b592ea9ba08b
szymoxD <szymongmyz1@vp.pl>
2025-04-15 18:34:56 +0200

    created photo adding form

--- src/profile.js
+++ src/profile.js
@@ -9,6 +9,14 @@ function Profile() {
         email: "",
     });
 
+    const [formData, setFormData] = useState({
+        title: "",
+        description: "",
+        is_private: false,
+        photo: null,
+    });
+    const [showModal, setShowModal] = useState(false);
+
     useEffect(() => {
         axios
             .get("http://localhost:5000/getuser", {
@@ -25,12 +33,78 @@ function Profile() {
             });
     }, []);
 
+    const handleChange = (e) => {
+        if(e.target.name=="photo")
+            setFormData({ ...formData, [e.target.name]: e.target.files[0] });
+        else
+            setFormData({ ...formData, [e.target.name]: e.target.value });
+
+    };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        
+        if (!formData.photo || !(formData.photo.type.split('/')[0]=="image")) {
+            sendWarning("Niedozwolony typ pliku!");
+            return;
+        }
+        try {
+            axios
+            .post("http://localhost:5000/addphoto",formData, {
+                headers: {
+                Authorization: localStorage.getItem("jwtToken"),
+                "Content-Type": "multipart/form-data"
+                },
+            })
+            .then((response) => {
+                if (response.status === 200) {
+                    sendError("Authentication failed");
+                    return;
+                }
+                else if(response.status === 201){
+                    console.log(response.data);
+                    sendSuccess("Photo added successfully!");
+                    setShowModal(false);
+                }else 
+                    sendError("Photo addings failed.");
+            })
+            .catch((error) => {
+                console.error("Błąd:", error);
+                sendError("Photo adding failed.");
+            });
+        } catch (error) {
+            sendError("Server error: " + error.message);
+        }
+    };
+
     return (
         <div className="page_container">
             <main className="content">
-                <h2>Profil</h2>
-                <p>Użytkownik: {userData.username}</p>
-                <p>Email: {userData.email}</p>
+                <div className="profile_card">
+                    <h2>Profil</h2>
+                    <p>Użytkownik: {userData.username}</p>
+                    <p>Email: {userData.email}</p>
+                    <button className="open-modal-btn" onClick={() => setShowModal(true)}>
+                        Dodaj zdjęcie
+                    </button>
+                </div>
+                {showModal && (
+                    <div className="modal_overlay">
+                        <div className="modal_content">
+                            <span className="close_modal" onClick={() => setShowModal(false)}>x</span>
+                            <h3>Dodaj zdjęcie</h3>
+                            <form onSubmit={handleSubmit} className="photo-form">
+                                <label>Tytuł:</label>
+                                <input type="text" name="title" onChange={handleChange} required />
+                                <label>Opis:</label>
+                                <textarea name="description" onChange={handleChange}/>
+                                <label><input type="checkbox" name="is_private" onChange={handleChange} /> Prywatne</label>
+                                <label>Zdjęcie:</label>
+                                <input type="file" name="photo" accept="image/*" onChange={handleChange} required />
+                                <button type="submit">Dodaj</button>
+                            </form>
+                        </div>
+                    </div>
+                )}
             </main>
         </div>
     );
